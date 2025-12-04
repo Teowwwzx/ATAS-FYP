3742f6b85c742efa13fcb9fe5dee6df07f6e7c1e
Teowwwzx <teowzx1@gmail.com>
2025-12-04 22:30:21 +0800

    Allow production domains in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,6 +27,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://thedzx.site",
+        "https://www.thedzx.site",
+        "https://atas-fyp-git-master-teowzxs-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
